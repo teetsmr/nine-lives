6621ad877c5157d83768f77c24bb4bd44cbf22a9
molly teets <teetsmr@miamioh.edu>
2025-12-07 18:45:34 +0100

    adding post processing effects
    chromatic aberration

--- nine-lives/Assets/Scripts/PauseManager.cs
+++ nine-lives/Assets/Scripts/PauseManager.cs
@@ -1,12 +1,22 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Rendering;       
+using UnityEngine.Rendering.Universal;
 
 public class PauseManager : MonoBehaviour
 {
     [SerializeField] GameObject pausePanel; 
-    [SerializeField] string mainMenuScene = "Main Menu"; 
+    [SerializeField] string mainMenuScene = "Main Menu";
+
+    [SerializeField] private Volume postVolume;
+    private ChromaticAberration ca;
 
     bool paused;
+
+    void Awake()
+    {
+        if (postVolume) postVolume.profile.TryGet(out ca); // cache the CA override if present
+    }
 
     void Update()
     {
@@ -21,6 +31,7 @@ public class PauseManager : MonoBehaviour
         if (pausePanel) pausePanel.SetActive(paused);
         Cursor.visible = paused;
         Cursor.lockState = paused ? CursorLockMode.None : CursorLockMode.Locked;
+        if (ca != null) ca.active = !paused;
     }
 
     public void GoToMainMenu()
